14f172c6ef5cde513837e3edce2599b762e790ae
Mictronics <github@mictronics.de>
2026-03-10 18:45:19 +0100

    Initial support for FLARM packets FLAC, FLAU, GRMZ.

--- packages/nmea-web-serial/src/parser/codecs/index.ts
+++ packages/nmea-web-serial/src/parser/codecs/index.ts
@@ -5,3 +5,6 @@
 export type { DBKPacket } from './DBK'
 export type { DBSPacket } from './DBS'
 export type { DPTPacket } from './DPT'
+export type { FLACPacket } from './FLAC'
+export type { FLAUPacket } from './FLAU'
+export type { GRMZPacket } from './GRMZ'
